314f5edc88227d3c85ba621151943e87a4c53921
Clark Winkelmann <clark.winkelmann@gmail.com>
2023-03-13 01:38:06 +0100

    Teams 2023

--- resources/js/components/Matches.ts
+++ resources/js/components/Matches.ts
@@ -104,7 +104,7 @@ export default class Matches implements m.ClassComponent<MatchesAttrs> {
                                     table.score ? [
                                         ' ',
                                         m('a.btn.btn-very-small.btn-outline-secondary', {
-                                            href: table.scoreboard ? 'https://fll-scoreboard-2018.robots-ju.ch/#' + table.scoreboard : '#',
+                                            href: table.scoreboard ? 'https://fll-scoreboard.robots-ju.ch/super-powered#' + table.scoreboard : '#',
                                             target: '_blank',
                                             rel: 'noopener',
                                             title: table.scoreboard ? 'Cliquer pour voir le détail des missions sur le scoreboard' : 'Scoreboard non disponible',
--- resources/js/teams.ts
+++ resources/js/teams.ts
@@ -21,54 +21,49 @@ export interface Team {
 
 const teams: Team[] = [
     {
+        key: 'jura1',
+        name: 'Jur\'arc électrique',
+        logo: 'ae.png',
+    },
+    {
+        key: 'jura2',
+        name: 'Ultra_powered.ju',
+        logo: 'ultrapoweredju.jpg',
+    },
+    {
+        key: 'jura3',
+        name: 'Les Patates jurassiennes',
+        logo: 'pj.png',
+    },
+    {
+        key: 'robohunter',
+        name: 'Robo-Hunter',
+        logo: 'robohunter.jpg',
+    },
+    {
+        key: 'juniorsmilebots',
+        name: 'Junior Smilebots',
+        logo: 'js.png',
+    },
+    {
+        key: 'cfrsblok',
+        name: 'CFR\'s Blok',
+        logo: 'cfrsblok.jpg',
+    },
+    {
+        key: 'phoenix',
+        name: 'Phoenix',
+        logo: 'phoenix.jpg',
+    },
+    {
         key: 'capricorns',
         name: 'Capricorns',
         logo: 'capricorns.png',
     },
     {
-        key: 'deepmind',
-        name: 'Deep Mind',
-        logo: 'dm.png',
-    },
-    {
-        key: 'falcon',
-        name: 'Falcon 9',
-        logo: 'f9.png',
-    },
-    {
-        key: 'jurapiter',
-        name: 'Jurapiter',
-        logo: 'jurapiter.jpg',
-    },
-    {
-        key: 'jurapollo',
-        name: 'Jur\'apollo 18',
-        logo: 'jurapollo18.png',
-    },
-    {
-        key: 'jurastronaute',
-        name: 'Jurastronaute',
-        logo: 'jurastronaute.jpg',
-    },
-    {
-        key: 'la5',
-        name: 'La 5',
-        logo: 'l5.png',
-    },
-    {
-        key: 'roch1',
-        name: 'Saint Roch I',
-        logo: 'roch.jpg',
-    },
-    {
-        key: 'roch2',
-        name: 'Saint Roch II',
-        logo: 'roch.jpg',
-    },
-    {
-        key: 'roch3',
-        name: 'Saint Roch 3',
-        logo: 'roch.jpg',
+        key: 'backtotheblock',
+        name: 'Back to the Block',
+        logo: 'backtotheblock.png',
     },
 ];
 
